4864a9cd129ceffb8945c03e6052cd9ff20fb2c5
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2020-06-25 20:54:00 +0900

    SETTING UP PRISMA

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 4000;
 
 
 const server = new GraphQLServer({schema});
+
 server.express.use(logger("dev"));
 server.start({port:PORT}, ()=>
 console.log(`✅  Server running on  http://localhost:${PORT}`))

0a99704c179ba3c0300a194961788fb089c81b78
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2020-07-02 16:24:37 +0900

    Computed Field - isSelf, isFollowing, isLiked

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -11,10 +11,5 @@ export default {
             const posts = await prisma.user({id:user.id}).post();
             return {user: userProfile, posts};
         }
-    },
-    User : {
-        fullName: parent => {
-        return `${parent.firstName} ${parent.lastName}`;
-        }
     }
-}
+};
